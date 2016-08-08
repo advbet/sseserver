fe8d70b4e83a0a5f14fe94d4bea2fa88a60df41c
Julius Kriukas <julius@kriukas.lt>
2016-08-08 17:08:40 +0300

    Add more detailed package documention

--- doc.go
+++ doc.go
@@ -1,8 +1,17 @@
 // Package ssestream is a library for creating SSE server HTTP handlers.
 //
-// This library provides a publish/subscribe interface for generating and
-// consuming SSE events. It handles SSE wire format, tracks active clients and
-// provides interface to resync clients on reconnects.
+// This library provides a publish/subscribe interface for generating SSE events
+// stream. It handles keep-alive messages, allows setting reconnect server and
+// client reconnect timeouts. Message data are always marshaled to JSON.
 //
-// This package encodes event data using JSON encoding.
+// This library provides an interface that allows resyncing client state after
+// reconnect.
+//
+// Typical usage of this package is - create new stream object with New(). Spawn goroutine
+// that creates events and publishes them via stream.Publish(). Create HTTP
+// handlers that unmarshal Last-Event-ID to native value and pass request
+// handling to library via stream.Subscribe(). Stitch everything together with a
+// resync function that allows looking up old events. If graceful shutdown or
+// dynamic stream creation is required use stream.Stop() to remove unused
+// streams.
 package ssestream
