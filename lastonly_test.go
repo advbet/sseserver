5b3313457e7f6e72db0485808d6635695f6dfb73
Julius Kriukas <julius@kriukas.lt>
2016-11-24 00:46:29 +0200

    Add tests for last event only stream

---
+++ lastonly_test.go
@@ -0,0 +1,34 @@
+package sseserver
+
+import (
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestLastOnlyResync(t *testing.T) {
+	stream := NewLastOnly(Config{
+		Reconnect:   0,
+		KeepAlive:   0,
+		Lifetime:    10 * time.Millisecond,
+		QueueLength: 32,
+	})
+	defer stream.Stop()
+
+	// Publish two events
+	event1 := &Event{ID: 16}
+	stream.Publish(event1)
+	event2 := &Event{ID: 32}
+	stream.Publish(event2)
+
+	// client should receive only last event
+	w := httptest.NewRecorder()
+	stream.Subscribe(w, 0)
+	assertReceivedEvents(t, w, event2)
+
+	// connect with up to date last event ID, should not generate duplicate
+	// events
+	w = httptest.NewRecorder()
+	stream.Subscribe(w, 32)
+	assertReceivedEvents(t, w)
+}
